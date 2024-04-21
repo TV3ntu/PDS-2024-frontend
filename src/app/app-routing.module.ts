6dbc9c4c9ab0b4d93f6e86bad2ef0c703b70b53c
ivoferdev <ivo.e.f@hotmail.com>
2024-04-21 00:38:49 -0300

    se crea la lista de instituciones y modifica un poco mas la estetica de la card institution

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router'
 import { AuthGuard } from './auth.guard'
 import { HomePageComponent } from './pages/home-page/home-page.component'
 import { InstitutionComponent } from './pages/institution/institution.component'
+import { InstitutionListComponent } from './pages/institution-list/institution-list.component'
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
-  { path: 'institution', component: InstitutionComponent }
+  { path: 'institution', component: InstitutionComponent },
+  { path: 'institutionlist',component:InstitutionListComponent}
   //TODO Agregar las rutas de los componentes
 ]
 @NgModule({
---
+++ src/app/pages/institution-list/institution-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { InstitutionComponent } from '../institution/institution.component';
+
+@Component({
+  selector: 'app-institution-list',
+  templateUrl: './institution-list.component.html',
+  styleUrls: ['./institution-list.component.css']
+})
+export class InstitutionListComponent {
+}
