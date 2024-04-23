8fb3754342dba659ed94fe1e27ad5978eb871d5e
tobiasriccone <tobi_riccone@hotmail.com>
2024-04-23 18:52:04 -0300

    Agrega componente footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import {MatCardModule} from '@angular/material/card';
 import { InstitutionComponent } from './pages/institution/institution.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    InstitutionComponent
+    InstitutionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
