69752ae17f0d244a57a9801a946c1be277aa25df
ivoferdev <ivo.e.f@hotmail.com>
2024-04-23 22:44:43 -0300

    creo institution component

--- src/app/pages/institution/institution.component.ts
+++ src/app/pages/institution/institution.component.ts
@@ -1,14 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Institution } from 'src/app/models/institution';
 
 @Component({
   selector: 'app-institution',
   templateUrl: './institution.component.html',
   styleUrls: ['./institution.component.css']
 })
-export class InstitutionComponent {
-    name = "title"
-    category = "subtitle"
-    image = "https://material.angular.io/assets/img/examples/shiba2.jpg"
-    description = "The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting."
-
+export class InstitutionComponent{
+  @Input()institution?: Institution// = new Institution("1","title","subtitle","The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.","")
 }
