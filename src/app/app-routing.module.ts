ef7625addf436e90ed3f1305177f8daf48a8cd38
agusnarvaez <agus.narvaez@outlook.com>
2024-04-26 16:06:02 -0300

    Corrijo ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { HomePageComponent } from './pages/home-page/home-page.component'
 import { EntityDetailComponent } from './pages/entity-detail/entity-detail.component'
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent, pathMatch: 'full'},
-  { path:'instituciones', component: HomePageComponent,pathMatch: 'full' },
-  { path:'insituciones/:id', component: EntityDetailComponent,pathMatch: 'full',data: { mode: 'institution' } },
-  { path:'cursos', component: HomePageComponent,pathMatch: 'full' },
-  { path:'cursos/:id', component: EntityDetailComponent,pathMatch: 'full',data: { mode: 'course' }},
+  /* { path: '', component: HomePageComponent, pathMatch: 'full'}, */
+  { path:'instituciones', component: HomePageComponent,pathMatch: 'full',data: { mode: 'institution' } },
+  { path:'cursos', component: HomePageComponent,pathMatch: 'full',data: { mode: 'course' } },
+  { path:'instituciones/:institutionId', component: EntityDetailComponent },
+  { path:'cursos/:courseId', component: EntityDetailComponent,pathMatch: 'full'},
+  { path:'', redirectTo: 'instituciones', pathMatch: 'full'}
   //TODO Agregar las rutas de los componentes
 ]
 @NgModule({
