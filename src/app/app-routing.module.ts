feb93bc137d35e58c4bfbcc13e0bb68891f40499
agusnarvaez <agus.narvaez@outlook.com>
2024-04-25 23:29:00 -0300

    Agrego routing a detalle de institucion y curso con modo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { AuthGuard } from './auth.guard'
 import { HomePageComponent } from './pages/home-page/home-page.component'
+import { EntityDetailComponent } from './pages/entity-detail/entity-detail/entity-detail.component'
 
 const routes: Routes = [
   { path: '', component: HomePageComponent, pathMatch: 'full'},
   { path:'instituciones', component: HomePageComponent,pathMatch: 'full' },
-  { path:'cursos', component: HomePageComponent,pathMatch: 'full' }
+  { path:'insituciones/:id', component: EntityDetailComponent,pathMatch: 'full',data: { mode: 'institution' } },
+  { path:'cursos', component: HomePageComponent,pathMatch: 'full' },
+  { path:'cursos/:id', component: EntityDetailComponent,pathMatch: 'full',data: { mode: 'course' }},
   //TODO Agregar las rutas de los componentes
 ]
 @NgModule({
