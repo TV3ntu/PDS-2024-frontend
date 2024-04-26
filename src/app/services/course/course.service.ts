13a828bd3eb01279e6aed0d966c01bab052c4376
agusnarvaez <agus.narvaez@outlook.com>
2024-04-26 00:55:46 -0300

    Corrijo categoría por descripción

--- src/app/services/course/course.service.ts
+++ src/app/services/course/course.service.ts
@@ -10,11 +10,11 @@ export class CourseService {
 
   constructor() {
     this.courses = [
-      new Course("1","Curso de Angular","Aprende Angular desde cero","Angular","https://wallpapers.com/images/hd/angular-js-logo-in-gray-7mrokd29izt1eyog.jpg"),
-      new Course("2","Curso de React","Aprende React desde cero","React","https://e1.pxfuel.com/desktop-wallpaper/556/915/desktop-wallpaper-how-to-install-reactjs-frontend.jpg"),
-      new Course("3","Curso de Vue","Aprende Vue desde cero","Vue","https://crisp.chat/static/blog/content/images/size/w2000/2023/03/migrar-vuejs.jpeg"),
-      new Course("4","Curso de Node","Aprende Node desde cero","Node","https://wallpapercave.com/wp/wp5070716.jpg"),
-      new Course("5","Curso de Python","Aprende Python desde cero","Python","https://wallpapercave.com/wp/wp7685924.jpg")
+      new Course("1","Curso de Angular","Angular","Aprende Angular desde cero","https://wallpapers.com/images/hd/angular-js-logo-in-gray-7mrokd29izt1eyog.jpg"),
+      new Course("2","Curso de React","React","Aprende React desde cero","https://e1.pxfuel.com/desktop-wallpaper/556/915/desktop-wallpaper-how-to-install-reactjs-frontend.jpg"),
+      new Course("3","Curso de Vue","Vue","Aprende Vue desde cero","https://crisp.chat/static/blog/content/images/size/w2000/2023/03/migrar-vuejs.jpeg"),
+      new Course("4","Curso de Node","Node","Aprende Node desde cero","https://wallpapercave.com/wp/wp5070716.jpg"),
+      new Course("5","Curso de Python","Python","Aprende Python desde cero","https://wallpapercave.com/wp/wp7685924.jpg")
     ]
   }
 
