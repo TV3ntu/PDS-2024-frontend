abb4e0f2696abd6fae63ed270d92edbd7ff117b0
agusnarvaez <agus.narvaez@outlook.com>
2024-04-25 23:03:46 -0300

    Heredo Course e institution de Entity

--- src/app/models/course.ts
+++ src/app/models/course.ts
@@ -1,16 +1,6 @@
-export class Course{
-    id: string
-    title: string
-    description: string
-    category: string
-    image: string
-    constructor(id: string, title: string, description: string, category: string, image: string){
-        this.id = id
-        this.title = title
-        this.description = description
-        this.category = category
-        this.image = image
-    }
+import { Entity } from "./entity"
+
+export class Course extends Entity{
 
     subscribe(){
         console.log("You've subscribed to course: " + this.title)
