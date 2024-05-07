94fe551adcb985b1779c6a50a08033f4aac32f04
ivoferdev <ivo.e.f@hotmail.com>
2024-05-07 00:08:37 -0300

    agrego al usuario metodo para desuscribirse

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,13 +1,23 @@
-export class Entity{
+import { Course } from "./course"
+
+export class User{
     id: string
     name: string
     lastName: string
     email: string
-    
-    constructor(id:string, name: string, lastName: string, email: string){
+    image: string
+    courses: Course[]
+    constructor(id:string, name: string, lastName: string, email: string,courses:[],image:string){
         this.id= id
         this.name=name
         this.lastName = lastName
         this.email = email
+        this.image = image
+        this.courses = courses
+    }
+
+    unsuscribe(course: Course){
+        var indice = this.courses.indexOf(course); // obtenemos el indice del curso
+        this.courses.splice(indice, 1)
     }
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {User} from 'src/app/models/user';
+import { Course } from 'src/app/models/course';
+import { courses } from 'src/app/mocks/mocks';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  constructor(private http: HttpClient) {}
+  user= new User("1","Juan","Carlos","juancarlos@gmail.com","https://t4.ftcdn.net/jpg/02/62/17/37/360_F_262173764_3sxll45SOaGP5uEC7PukV3LHOB7H8dp2.jpg",courses)
+  constructor(private http: HttpClient) {
+    
+  }
+
+  Unsuscribe(course:Course){this.user.unsuscribe(course)}
+
 }
