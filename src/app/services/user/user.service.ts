6b0e3999a5804e336b2bf9008ae907eb389b3aa2
tobiasriccone <tobi_riccone@hotmail.com>
2024-05-09 20:57:17 -0300

    Agrega user en variable estatica de userService

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {Observable, tap} from "rxjs";
 import {User} from "../../models/user";
 
 @Injectable({
@@ -8,11 +8,13 @@ import {User} from "../../models/user";
 })
 export class UserService {
   serverUrl = 'http://localhost:8080'
+  public static currentUser: User | null = null
 
   constructor(private http: HttpClient) {}
 
   login(email: string, password: string): Observable<User> {
     const body = { email, password }
     return this.http.post<any>(this.serverUrl + '/api/users/login', body)
+      .pipe( tap(user => UserService.currentUser = user) )
   }
 }
