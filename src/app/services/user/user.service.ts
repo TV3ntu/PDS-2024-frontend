0f2f696055c98aaede18084294ce9bfa404585a2
agusnarvaez <agus.narvaez@outlook.com>
2024-05-10 15:53:46 -0300

    Agrego métodos de obtenciónd el user

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable, tap} from "rxjs";
+import {BehaviorSubject, Observable, tap} from "rxjs";
 import {User} from "../../models/user";
 
 @Injectable({
@@ -13,19 +13,36 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   unsuscribe(){}
-  getUser = () => this.currentUser
+  getLoggedUser = () => this.currentUser
 
   login(email: string, password: string): Observable<User> {
     const body = { email, password }
     return this.http.post<any>(this.serverUrl + '/api/users/login', body)
-      .pipe( tap(user => this.currentUser = user) )
+      .pipe( tap(user => {
+        localStorage.setItem('userId', user.id)
+        this.currentUser = user
+        console.log(user.id)
+      }))
   }
 
   logout() {
+    localStorage.removeItem('userId')
+    localStorage.clear()
     this.currentUser = null
+    console.log('logged out')
+  }
+  getUserLoggedData(): Observable<User> {
+    return this.http.get<User>(this.serverUrl + '/api/users/' + localStorage.getItem('userId'))
   }
 
-  isLogged = () => this.currentUser != null
+  isLogged = (): boolean => {
+    console.log(localStorage.getItem('userId'),(localStorage.getItem('userId')==null || localStorage.getItem('userId') == undefined ))
+    return localStorage.getItem('userId') != null
+  }
+
+  getById(id: string): Observable<User> {
+    return this.http.get<User>(this.serverUrl + '/api/users/' + id + '/detail')
+  }
 
   updateUser(user: User): Observable<User> {
     return this.http.patch<User>(this.serverUrl + '/api/users/' + user.id + '/detail', user)
