55174650d3e7d2fced97415fc8306b8e58678f20
agusnarvaez <agus.narvaez@outlook.com>
2024-05-10 15:52:32 -0300

    Agrego validación de si está logueado en auth

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -9,12 +9,6 @@ export class AuthGuard implements CanActivate {
   constructor(private router: Router,private userService:UserService) {}
 
   canActivate(): boolean {
-    // TODO: Implement your authentication logic here
-    /* if (localStorage.getItem('userId')) {
-      return true
-    }
-    this.router.navigate(['/login'])
-    return false */
     if(!this.userService.isLogged()) {
       this.router.navigate(['/ingresar'])
       return false
