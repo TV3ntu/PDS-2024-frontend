e480a6f870c9a4fdfdd6cc95168f76475e751fc2
agusnarvaez <agus.narvaez@outlook.com>
2024-05-10 11:18:25 -0300

    Maqueto form de login con errores

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/services/user/user.service';
 
+interface UserLogin{
+  email: string
+  password: string
+
+}
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
+  @ViewChild('loginForm') loginForm!: NgForm
+  user: UserLogin = {email: '', password: ''}
 
+  constructor(private userService:UserService) { }
+
+  login() {
+    this.userService.login(this.user.email, this.user.password)
+  }
 }
