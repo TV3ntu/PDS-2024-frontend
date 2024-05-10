2024ceeaecdfe5a8100cfc623a7dc44b58f3914c
agusnarvaez <agus.narvaez@outlook.com>
2024-05-10 14:58:09 -0300

    Agrego profile page, redirección y navegación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { AuthGuard } from './auth.guard'
 import { HomePageComponent } from './pages/home-page/home-page.component'
 import { EntityDetailComponent } from './pages/entity-detail/entity-detail.component'
 import { LoginPageComponent } from './pages/login-page/login-page.component'
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component'
 
 const routes: Routes = [
   /* { path: '', component: HomePageComponent, pathMatch: 'full'}, */
   { path:'ingresar', component: LoginPageComponent,pathMatch: 'full' },
+  { path:'perfil', component: ProfilePageComponent, canActivate: [AuthGuard]},
   { path:'instituciones', component: HomePageComponent,pathMatch: 'full',data: { mode: 'institution' } },
   { path:'cursos', component: HomePageComponent,pathMatch: 'full',data: { mode: 'course' } },
   { path:'instituciones/:institutionId', component: EntityDetailComponent },
--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core'
 import { CanActivate, Router } from '@angular/router'
+import { UserService } from './services/user/user.service'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) {}
+  constructor(private router: Router,private userService:UserService) {}
 
   canActivate(): boolean {
     // TODO: Implement your authentication logic here
@@ -14,6 +15,10 @@ export class AuthGuard implements CanActivate {
     }
     this.router.navigate(['/login'])
     return false */
+    if(!this.userService.isLogged()) {
+      this.router.navigate(['/ingresar'])
+      return false
+    }
     return true
   }
 }
