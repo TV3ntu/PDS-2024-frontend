03c9a7561810592ecbed4fa8f1e613d1728f4422
tobiasriccone <tobi_riccone@hotmail.com>
2024-05-24 19:01:56 -0300

    Implementa el filtrado de los cursos

--- src/app/services/course/course.service.ts
+++ src/app/services/course/course.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Course } from 'src/app/models/course';
-import {courses, institutions} from 'src/app/mocks/mocks'
-import { InstitutionService } from '../institution/institution.service';
+import { institutions } from 'src/app/mocks/mocks'
 import { Institution } from 'src/app/models/institution';
 import { path } from '../api.path';
 import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
   path: string = path.local + '/api/courses'
-
   courses: Course[]
   institutions: Institution[]
 
@@ -23,12 +22,12 @@ export class CourseService {
   getById(id: string): Observable<Course|undefined> {
     return this.http.get<Course>(this.path + '/api/courses/' + id)
   }
- // getById = (id:string):Observable<Course | undefined> => of(this.courses.find(c => c.id == id))
 
-  getAll = ():Observable<Course[]> => this.http.get<Course[]>(this.path)
+  getAll(filter: String = ""): Observable<Course[]> {
+    return this.http.get<Course[]>(this.path + '?query=' + filter)
+  }
 
   getAllFromInstitution = (institutionID:string):Observable<Course[] | undefined> => of(this.institutions.find(i => i.id == institutionID)?.children)
 
   getByCategory = (category:string):Course[] => this.courses.filter(c => c.category == category)
-
 }
