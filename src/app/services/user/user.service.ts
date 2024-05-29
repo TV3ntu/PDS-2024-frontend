5f89debbf9214de0d7354583e0cfdef38a5fbe80
agusnarvaez <agus.narvaez@outlook.com>
2024-05-28 22:13:34 -0300

    Comento métodos incompletos

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -41,9 +41,9 @@ export class UserService {
 
   getById = (id: string): Observable<User> => this.http.get<User>(this.serverUrl + '/api/users/' + id)
 
-  subscribe = (user:User, assignment:Assignment): Observable<User> => this.http.post<User>(this.serverUrl + '/api/users/subscribe', {user, assignment})
+  //subscribe = (user:User, assignment:Assignment): Observable<User> => this.http.post<User>(this.serverUrl + '/api/users/subscribe', {user, assignment})
 
-  unsuscribe = (user:User, assignment:Assignment): Observable<User> => this.http.post<User>(this.serverUrl + '/api/users/unsubscribe', {user, assignment})
+  //unsuscribe = (user:User, assignment:Assignment): Observable<User> => this.http.post<User>(this.serverUrl + '/api/users/unsubscribe', {user, assignment})
 
   updateUser = (user: User): Observable<User> => this.http.patch<User>(this.serverUrl + '/api/users/' + user.id, user)
 
