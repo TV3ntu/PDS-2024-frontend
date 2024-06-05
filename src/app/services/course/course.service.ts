371b309b9802b1bac3130c8a526188c3673f5fa6
ivoferdev <ivo.e.f@hotmail.com>
2024-06-05 20:47:30 -0300

    modificación deleteAll

--- src/app/services/course/course.service.ts
+++ src/app/services/course/course.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
 import { Course } from 'src/app/models/course'
 import { path } from '../api.path'
-import { HttpClient } from '@angular/common/http'
+import { HttpClient,HttpParams } from '@angular/common/http'
 import { Assignment } from 'src/app/models/assignment'
 
 interface CourseResponse{
@@ -29,6 +29,15 @@ export class CourseService {
 
   delete = (course:Course) => this.http.delete<Course>(this.path+'/api/courses/'+course.id)
 
-  deleteAll = (filter: String = ""): Observable<Course[]> => this.http.delete<Course[]>(this.path + '?query=' + filter)
+  deleteAll(courses: string[]): Observable<Course[]> {
+  let params = new HttpParams();
+  courses.forEach(course => {
+  params = params.append('idCourses', course);
+  });
+  
+  return this.http.delete<Course[]>(`${this.path}/api/courses`, { params });
+  
+  }
+  
 
 }
