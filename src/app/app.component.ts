b511ad6d50c559c14b6d3d54318fce3c605642cb
agusnarvaez <agus.narvaez@outlook.com>
2024-06-06 20:05:19 -0300

    Corrijo title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tus Horarios'
+  title = 'WeekBook'
 
   constructor(private router: Router){}
 
