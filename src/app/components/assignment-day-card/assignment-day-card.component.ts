d6dcdc6d2368473aeb7a2dc5ea13b301eeaa381d
agusnarvaez <agus.narvaez@outlook.com>
2024-06-11 22:08:38 -0300

    Hacemos que show venga por input

--- src/app/components/assignment-day-card/assignment-day-card.component.ts
+++ src/app/components/assignment-day-card/assignment-day-card.component.ts
@@ -14,7 +14,7 @@ import { UserService } from 'src/app/services/user/user.service';
 })
 export class AssignmentDayCardComponent {
   @Input() assignment!: Assignment
-  showDay: boolean = true
+  @Input() showDay: boolean = true
 
   constructor(
     private router:Router,
