a3f2de620a84502e5f503117ff2a7f027dcde657
agusnarvaez <agus.narvaez@outlook.com>
2024-06-11 22:10:14 -0300

    Realizo nuevamente la petición de curso cuando se ejecuta la notificación

--- src/app/pages/entity-detail/entity-detail.component.ts
+++ src/app/pages/entity-detail/entity-detail.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router'
 import { Entity } from 'src/app/models/entity';
 import { CourseService } from 'src/app/services/course/course.service'
 import { InstitutionService } from 'src/app/services/institution/institution.service'
+import { NotificationService } from 'src/app/services/notification/notification.service';
 
 @Component({
   selector: 'app-entity-detail',
@@ -17,8 +18,14 @@ export class EntityDetailComponent {
   constructor(
     private courseService:CourseService,
     private institutionService: InstitutionService,
-    private route:ActivatedRoute
-  ){ }
+    private route:ActivatedRoute,
+    private notificationService:NotificationService
+  ){
+    this.notificationService.notification$.subscribe(()=>{
+      if(this.isInstitution()) this.getInstitution()
+      if(!this.isInstitution()) this.getCourse()
+    })
+   }
 
   ngOnInit(){
     // Obtengo el modo de la entidad a mostrar
