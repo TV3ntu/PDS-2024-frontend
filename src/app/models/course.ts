125107b41ca82d1c864a5dd4c97a961ae4c027ff
ivoferdev <ivo.e.f@hotmail.com>
2024-06-14 21:01:46 -0300

    upgraded the visualization

---
+++ src/app/models/course.ts
@@ -0,0 +1,22 @@
+import { Assignment } from "./assignment"
+import { Entity } from "./entity"
+
+export class Course extends Entity{
+    institutionId: string = ''
+
+    subscribe(){
+        console.log("You've subscribed to course: " + this.title)
+    }
+
+    unsubscribe(){
+        console.log("You've unsubscribed from course: " + this.title)
+    }
+
+    override children: Assignment[]
+
+    constructor(id:string, title: string, description: string, image: string,category:string, children: Assignment[]){
+        super(id, title,category, description,image)
+
+        this.children = children
+    }
+}
