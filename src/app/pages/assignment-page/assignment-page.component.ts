86b0cd09f1a31a438a53d6a35adbcdac800e10ff
ivoferdev <ivo.e.f@hotmail.com>
2024-06-23 17:21:25 -0300

    finish validation of assignmentform

--- src/app/pages/assignment-page/assignment-page.component.ts
+++ src/app/pages/assignment-page/assignment-page.component.ts
@@ -5,6 +5,8 @@ import { NotificationService } from 'src/app/services/notification/notification.
 import { AssignmentService } from 'src/app/services/assignment/assignment.service';
 import { Schedule } from 'src/app/models/schedule';
 import { catchError } from 'rxjs';
+import { DatePipe } from '@angular/common';
+import { formatDate } from "@angular/common";
 
 
 interface NewAssignment{
@@ -39,20 +41,18 @@ export class assignmentPageComponent {
   atLeastOneDaySelected: boolean=false;
   //flag to don't display error when enter at page
   newTimeOnPage: boolean=true
-  invalidDate: boolean=false
-  currentDate: Date= new Date()
   courseId: string = ''
 
 
   assignment: NewAssignment = {
     id:'',
-    quantityAvailable: 0,
+    quantityAvailable: 10,
     isActive:true,
-    price: 0,
+    price: 100,
     startDate: new Date(),
     endDate: new Date(),
     days:[],
-    startTime: new Date("00:00:00"),
+    startTime:new Date("00:00:00"),
     endTime: new Date("00:00:00"),
     recurrenceWeek:'',
     listDates:[],
@@ -60,10 +60,17 @@ export class assignmentPageComponent {
   }
 
   constructor(private route: ActivatedRoute, private router: Router,
-              private assignmentService:AssignmentService, private notificationService: NotificationService) { }
+              private assignmentService:AssignmentService, 
+              private notificationService: NotificationService,
+              private datePipe: DatePipe
+            ) {}
 
   ngOnInit() {
     this.courseId = this.route.snapshot.paramMap.get('courseId')!;
+    //para que no se pueda crear un assigment para el mismo dia
+    this.assignment.startDate.setDate(this.assignment.startDate.getDate() + 1)
+        // para que almenos el curso cierre un dia despues que el dia de creado
+    this.assignment.endDate.setDate(this.assignment.endDate.getDate() + 2)
   }
 
   daySelectedCheck() {
@@ -101,10 +108,51 @@ export class assignmentPageComponent {
     if(this.fridaySelected) this.assignment.days.push('FRIDAY')
     if(this.saturdaySelected) this.assignment.days.push('SATURDAY')
     if(this.sundaySelected) this.assignment.days.push('SUNDAY')
+    new Set(this.assignment.days)
   }
 
   setSchedule(){
     this.assignment.schedule = new Schedule(this.assignment.days,String(this.assignment.startTime),String(this.assignment.endTime),String(this.assignment.startDate),String(this.assignment.endDate),this.assignment.recurrenceWeek,this.assignment.listDates)
+    console.log(this.assignment.schedule)
   }
 
+  checkQuantityValue(){
+    return this.assignment.quantityAvailable==0
   }
+
+  checkPriceValue(){
+    return this.assignment.price==0
+  }
+
+
+  minEndDateValue(){
+    //assign the startDate like the minimun value possible in the form
+    return this.datePipe.transform(this.assignment.endDate, 'yyyy-MM-dd')
+  }
+
+  minStartDateValue(){
+    //assign the current date how the minimun value possible in the form
+      let tomorrowDate= new Date()  
+      tomorrowDate.setDate(tomorrowDate.getDate() + 1)
+      return this.datePipe.transform(tomorrowDate, 'yyyy-MM-dd')     
+  }
+
+  checkStartDateValue(){
+    let startDateInput= new Date(this.assignment.startDate)
+    let currentDate= new Date()
+    //devuelve si la fecha de comienzo es anterior a la fecha actual
+    return startDateInput<=currentDate
+  }
+
+  checkEndDateValue(){
+    let startDateInput= new Date(this.assignment.startDate)
+    let endDateInput= new Date(this.assignment.endDate)
+    //devuelve si la fecha final es anterior a la decha de comienzo
+    return endDateInput<=startDateInput
+  }
+
+  minEndTimeValue(){
+    //devuelve si la fecha de comienzo es posterior a la decha de comienzo
+    return this.assignment.startTime>this.assignment.endTime
+  }
+}
