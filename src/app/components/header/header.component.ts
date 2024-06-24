f2eabfe2cc8e9b3a011c0a7a5e29ef53085bfe85
ivoferdev <ivo.e.f@hotmail.com>
2024-06-23 21:41:50 -0300

    Added possibility to delete an user

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -31,11 +31,15 @@ export class HeaderComponent {
   }
 
   checkUser (){
+    console.log(this.userService.isLogged())
+    console.log(localStorage.getItem('userId'))
     if(this.userService.isLogged()){
       this.isLogged = true
       this.userService.getUserLoggedData().subscribe(user => {
         this.user = user
       })
+    } else{
+      this.isLogged=false
     }
   }
 
